48877cd1c54d1630f230aee0143e36fad3e55511
Anicet Nougaret <anicet.nougaret@zohomail.eu>
2021-07-19 22:55:42 +0200

    improved

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { resolve } from 'path';
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +11,11 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: adapter(),
+		adapter: adapter({
+            pages: './build',
+			assets: './build',
+			fallback: null
+        }),
         vite: {
             resolve: {
                 alias: {
