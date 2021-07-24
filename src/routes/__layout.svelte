2aff93399bc852cf1195f31b81a426797615b459
Anicet Nougaret <anicet.nougaret@zohomail.eu>
2021-07-24 21:41:43 +0200

    added thumbnail and meta

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -11,6 +11,17 @@
     });
 </script>
 
+<svelte:head>
+    <meta name="application-name" content="Anicet Nougaret's portfolio."/>
+    <meta
+        name="keywords"
+        content="ELIXIR, SVELTE, DEV, FRANCE, PARIS, LYON, INSA, IUT, INFORMATIQUE, JAVASCRIPT, TYPESCRIPT, REACT, NODE, EUROPE, STUDENT"
+    />
+    <title>Anicet Nougaret</title>
+    <meta name="description" content="Anicet Nougaret's portfolio. CS Student, game creator, pixelartist."/>
+    <meta property="og:image" content="/images/thumbnail.jpg"/>
+</svelte:head>
+
 <main class="main-theme light-theme theme-compute" class:unmounted>
     <slot/>
 </main>
